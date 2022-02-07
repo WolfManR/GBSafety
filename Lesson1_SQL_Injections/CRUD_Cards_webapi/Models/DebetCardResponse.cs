6f6a2e1c9271f7942a9fb0ff0d36cd1038d283ca
Tarxos <ftarxos@gmail.com>
2022-02-08 01:26:09 +0300

    add CRUD request, response models

---
+++ Lesson1_SQL_Injections/CRUD_Cards_webapi/Models/DebetCardResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace CRUD_Cards_webapi.Models;
+
+internal sealed class DebetCardResponse : DebetCardBase
+{
+    public int Id { get; init; }
+}
---
+++ Lesson1_SQL_Injections/CRUD_Cards_webapi/Models/UpdateDebetCardRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace CRUD_Cards_webapi.Models;
+
+internal sealed class UpdateDebetCardRequest : DebetCardBase
+{
+
+}
