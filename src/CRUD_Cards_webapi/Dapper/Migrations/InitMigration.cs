b535cea478ee6870fa9d7b7210252ff492352986
Tarxos <ftarxos@gmail.com>
2022-02-18 01:48:45 +0300

    add simple migration for dapper database

---
+++ src/CRUD_Cards_webapi/Dapper/Migrations/InitMigration.cs
@@ -0,0 +1,22 @@
+﻿using FluentMigrator;
+
+namespace CRUD_Cards_webapi.Dapper.Migrations;
+
+[Migration(14218022022)]
+public class InitMigration : Migration
+{
+    public override void Up()
+    {
+        Create.Table("DebetCards")
+            .WithColumn("Id").AsGuid().PrimaryKey().Identity()
+            .WithColumn("Number").AsString()
+            .WithColumn("Holder").AsString()
+            .WithColumn("ExpireMonth").AsInt32()
+            .WithColumn("ExpireYear").AsInt32();
+    }
+
+    public override void Down()
+    {
+        Delete.Table("DebetCards");
+    }
+}
