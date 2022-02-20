21c784aa84e285239646d6a9652bdf8113f79091
Tarxos <ftarxos@gmail.com>
2022-02-21 01:19:39 +0300

    register services in di container

--- src/CRUD_Cards_webapi/Program.cs
+++ src/CRUD_Cards_webapi/Program.cs
@@ -1,7 +1,11 @@
+using CRUD_Cards_webapi.Dapper;
+using CRUD_Cards_webapi.Dapper.Migrations;
 using CRUD_Cards_webapi.EF;
 using CRUD_Cards_webapi.Models;
 using CRUD_Cards_webapi.Services;
 using CRUD_Cards_webapi.Validations;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Thundire.Helpers;
@@ -12,9 +16,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<CardsDbContext>((p, o) => o.UseNpgsql(p.GetRequiredService<IConfiguration>().GetConnectionString("Postgree")));
+builder.Services.AddDbContext<CardsDbContext>((p, o) => o.UseNpgsql(p.GetRequiredService<IConfiguration>().GetConnectionString("PostgreeEF")));
 
-builder.Services.AddScoped<IDebetCardsService, DebetCardsService>();
+builder.Services
+    .AddScoped<InitMigration>()
+    .AddScoped<DapperDebetCardsRepository>()
+    .AddScoped<CardsDapperDbContext>();
+
+builder.Services
+    .AddScoped<EFCoreDebetCardsRepository>()
+    .AddScoped<EFCoreDebetCardsService>();
+
+builder.Services.AddFluentValidation();
+builder.Services.AddScoped<IValidator<DebetCardBase>, DebetCardValidation>();
 
 var app = builder.Build();
 
@@ -31,13 +45,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapGet("debet", static async (IDebetCardsService debetCardsService) =>
+app.MapGet("debet", static async (EFCoreDebetCardsService debetCardsService) =>
 {
     var data = await debetCardsService.Get();
     return Results.Ok(data);
 });
 
-app.MapGet("debet/{id}", static async ([FromRoute] int id, IDebetCardsService debetCardsService) =>
+app.MapGet("debet/{id}", static async ([FromRoute] int id, EFCoreDebetCardsService debetCardsService) =>
 {
     if (id <= 0)
     {
@@ -54,14 +68,14 @@ app.MapGet("debet/{id}", static async ([FromRoute] int id, IDebetCardsService de
     return Results.Ok(successResult.CallBackData);
 });
 
-app.MapDelete("debet/{id}", static async ([FromRoute] int id, IDebetCardsService debetCardsService) =>
+app.MapDelete("debet/{id}", static async ([FromRoute] int id, EFCoreDebetCardsService debetCardsService) =>
 {
     if (id <= 0) return Results.BadRequest();
     await debetCardsService.Delete(id);
     return Results.Ok();
 });
 
-app.MapPut("debet/{id}", static async ([FromRoute] int id, [FromBody] UpdateDebetCardRequest request, IDebetCardsService debetCardsService, DebetCardValidation validation) =>
+app.MapPut("debet/{id}", static async ([FromRoute] int id, [FromBody] UpdateDebetCardRequest request, EFCoreDebetCardsService debetCardsService, IValidator<DebetCardBase> validation) =>
 {
     if (id <= 0) return Results.BadRequest();
 
@@ -73,7 +87,7 @@ app.MapPut("debet/{id}", static async ([FromRoute] int id, [FromBody] UpdateDebe
     return !result.IsSuccess ? Results.NotFound() : Results.Ok();
 });
 
-app.MapPost("debet", static async ([FromBody] CreateDebetCardRequest request, IDebetCardsService debetCardsService, DebetCardValidation validation) =>
+app.MapPost("debet", static async ([FromBody] CreateDebetCardRequest request, EFCoreDebetCardsService debetCardsService, IValidator<DebetCardBase> validation) =>
 {
     var validationResult = await validation.ValidateAsync(request);
     if (!validationResult.IsValid) return Results.BadRequest();
