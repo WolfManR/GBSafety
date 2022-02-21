09aac5c874eaf7839120b366a7cae0b35798657b
Tarxos <ftarxos@gmail.com>
2022-02-22 01:29:15 +0300

    fix validation for year and month

--- src/CRUD_Cards_webapi/Validations/DebetCardValidation.cs
+++ src/CRUD_Cards_webapi/Validations/DebetCardValidation.cs
@@ -8,13 +8,18 @@ internal sealed class DebetCardValidation : AbstractValidator<DebetCardBase>
     public DebetCardValidation()
     {
         var date = DateTime.UtcNow;
-        RuleFor(x => x.ExpireMonth)
-            .GreaterThanOrEqualTo(date.Month).WithMessage("Month cannot be expired")
-            .LessThanOrEqualTo(12).WithMessage("In year for now only 12 month");
+        var month = date.Month;
+        var year = date.Year / 100;
 
-        RuleFor(x => x.ExpireYear).GreaterThanOrEqualTo(date.Year).WithMessage("You cannot add expired card");
 
-        RuleFor(x => x.Number).CreditCard().WithMessage("Not valid card number");
+        RuleFor(x => x.ExpireMonth).InclusiveBetween(month, 12).WithMessage("Month cannot be expired");
+
+        RuleFor(x => x.ExpireYear).GreaterThanOrEqualTo(year).WithMessage("You cannot add expired card");
+
+        RuleFor(x => x.Number)
+            //.NotNull().WithMessage("It hard to generate card number so it just must exist")
+            .CreditCard().WithMessage("Not valid card number")
+            ;
 
         // Not checked
         RuleFor(x => x.Holder).Matches("[a-zA-Z ]+").WithMessage("Not a valid character in holder name");
