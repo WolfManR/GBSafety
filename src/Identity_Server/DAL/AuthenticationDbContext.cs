892018b5afd766fa6c54ecda3d4783b02451667f
Tarxos <ftarxos@gmail.com>
2022-02-26 03:09:21 +0300

    rename identity db context to AuthenticationDbContext to that it not be misspeled with ms context

---
+++ src/Identity_Server/DAL/AuthenticationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity_Server.DAL;
+
+internal sealed class AuthenticationDbContext : IdentityDbContext<AuthenticationUser>
+{
+    public AuthenticationDbContext(DbContextOptions<AuthenticationDbContext> options) : base(options)
+    {
+
+    }
+}
