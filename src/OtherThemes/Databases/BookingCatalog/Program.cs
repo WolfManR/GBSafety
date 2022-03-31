a01f6752d201f143815222f91f247c76647d28da
Tarxos <ftarxos@gmail.com>
2022-03-31 12:00:49 +0300

    add blank endpoint for testing work of repository

--- src/OtherThemes/Databases/BookingCatalog/Program.cs
+++ src/OtherThemes/Databases/BookingCatalog/Program.cs
@@ -1,9 +1,28 @@
+using BookingCatalog.Core;
+using BookingCatalog.Graph_Neo4j_Data;
+using BookingCatalog.Graph_Neo4j_Data.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services
+    .AddSingleton<Neo4jOptions>(provider =>
+    {
+        var configuration = provider.GetRequiredService<IConfiguration>();
+
+        return new Neo4jOptions()
+        {
+            Uri = configuration.GetValue<string>("ConnectionStrings:Neo4j:Uri"),
+            Login = configuration.GetValue<string>("ConnectionStrings:Neo4j:Login"),
+            Password = configuration.GetValue<string>("ConnectionStrings:Neo4j:Password"),
+        };
+    })
+    .AddScoped<Neo4jContext>()
+    .AddScoped<IBooksRepository, BooksRepository>();
 
 var app = builder.Build();
 
@@ -13,4 +32,16 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MapPost("books/store", StoreBooks);
+
 app.Run();
+
+
+async Task<IResult> StoreBooks(StoreBooksRequest request, IBooksRepository booksRepository)
+{
+   await booksRepository.Store(request.Book, request.Author, request.Amount);
+   return Results.Ok();
+}
+
+record StoreBooksRequest(Book Book, Author Author, int Amount);
