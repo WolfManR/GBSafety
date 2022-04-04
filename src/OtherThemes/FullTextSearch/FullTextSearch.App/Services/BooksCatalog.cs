b3a0d62306856c035afdd8ffdb0d49d8d7a4bd36
Tarxos <ftarxos@gmail.com>
2022-04-04 15:49:49 +0300

    make base repository

---
+++ src/OtherThemes/FullTextSearch/FullTextSearch.App/Services/BooksCatalog.cs
@@ -0,0 +1,18 @@
+﻿using FullTextSearch.App.Models;
+
+namespace FullTextSearch.App.Services;
+
+public class BooksCatalog
+{
+    public static HashSet<Book> Books { get; set; }
+
+    public void Add(Book book)
+    {
+        Books.Add(book);
+    }
+
+    public List<Book> List()
+    {
+        return Books.ToList();
+    }
+}
