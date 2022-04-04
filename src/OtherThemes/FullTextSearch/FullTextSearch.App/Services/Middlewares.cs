a6225145190b14caf842601f56d73ee054f4dd5d
Tarxos <ftarxos@gmail.com>
2022-04-04 18:30:44 +0300

    add middleware that will fill catalog with books

---
+++ src/OtherThemes/FullTextSearch/FullTextSearch.App/Services/Middlewares.cs
@@ -0,0 +1,17 @@
+﻿namespace FullTextSearch.App.Services;
+
+public static class Middlewares
+{
+    public static IApplicationBuilder FillRepository(this IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+        var generate = scope.ServiceProvider.GetRequiredService<BookGenerator>();
+        var catalog = scope.ServiceProvider.GetRequiredService<BooksCatalog>();
+
+        foreach (var book in generate.InCount(24))
+        {
+            catalog.Add(book);
+        }
+        return app;
+    }
+}
