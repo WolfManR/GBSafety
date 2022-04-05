4f77649eb4ef96e8c3a9c3bbec2974b5f4fc8976
Tarxos <ftarxos@gmail.com>
2022-04-06 00:21:24 +0300

    add console client project with sample code

---
+++ src/OtherThemes/ServiceDiscovery/ServiceDiscovery/ConsoleClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleClient.LoadBalancers;
+using ConsoleClient.ServiceProviders;
+
+var client = new HttpClient();
+ILoadBalancer balancer = new RoundRobinLoadBalancer(new PollingConsulServiceProvider());
+// Вызов с использованием алгоритма опроса
+for (int i = 0; i < 10; i++)
+{
+    var service = await balancer.GetServiceAsync();
+    Console.WriteLine(DateTime.Now + "-RoundRobin:" +
+                      await client.GetStringAsync("http://" + service + "/api/values") + " --> Request from " + service);
+}
+// Используем случайный алгоритм вызова
+balancer = new RandomLoadBalancer(new PollingConsulServiceProvider());
+for (int i = 0; i < 10; i++)
+{
+    var service = await balancer.GetServiceAsync();
+    Console.WriteLine(DateTime.Now + "-Random:" +
+                      await client.GetStringAsync("http://" + service + "/api/values") + " --> Request from " + service);
+}
---
+++ src/OtherThemes/ServiceDiscovery/ServiceDiscovery/ConsoleClient/ServiceProviders/ConsulServiceProvider.cs
@@ -0,0 +1,21 @@
+﻿using Consul;
+
+namespace ConsoleClient.ServiceProviders;
+
+public class ConsulServiceProvider : IServiceDiscoveryProvider
+{
+    public async Task<List<string>> GetServicesAsync()
+    {
+        var consulClient = new ConsulClient(consulConfig =>
+        {
+            consulConfig.Address = new Uri("http://localhost:8500");
+        });
+        var queryResult = await consulClient.Health.Service("Service", string.Empty, true);
+        List<string> result = new();
+        foreach (var serviceEntry in queryResult.Response)
+        {
+            result.Add(serviceEntry.Service.Address + ":" + serviceEntry.Service.Port);
+        }
+        return result;
+    }
+}
